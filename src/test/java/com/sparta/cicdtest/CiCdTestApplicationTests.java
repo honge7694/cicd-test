04e1dba9d6a2982f7c90783f65880dc7a7364f07
hongui <honge7694@naver.com>
2023-11-22 17:02:44 +0900

    Update

--- src/test/java/com/sparta/cicdtest/CiCdTestApplicationTests.java
+++ src/test/java/com/sparta/cicdtest/CiCdTestApplicationTests.java
@@ -3,7 +3,7 @@ package com.sparta.cicdtest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CiCdTestApplicationTests {
 
     @Test
